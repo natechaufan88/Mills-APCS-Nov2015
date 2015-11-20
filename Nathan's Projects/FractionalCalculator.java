ed8ec40a593e2cfc740b3674fd8330c5d9709bc1
natechaufan88 <natechaufan88@gmail.com>
2015-11-20 08:44:09 -0800

    FracCalc

--- Nathan's Projects/FractionalCalculator.java
+++ Nathan's Projects/FractionalCalculator.java
@@ -9,17 +9,18 @@ import java.util.*;
 public class FractionalCalculator {
     public static void main(String[] args) {
         Scanner userinput = new Scanner(System.in);
-        System.out.print("What is your expression? ");
-        String expression = userinput.nextLine();
-        while (!(expression.equals("quit"))) {
+        System.out.print("What is your expression? "); // prompt user for input
+        String expression = userinput.nextLine(); // user types in input
+        while (!(expression.equals("quit"))) { // while user input is not "quit"
             System.out.println(produceAnswer(expression));
-            System.out.print("What is your expression? ");
-            expression = userinput.nextLine();
+            System.out.print("What is your expression? "); // prompt user for input
+            expression = userinput.nextLine(); // user types in input
         }
         System.out.println("You may now exit.");
     }
     
     public static String produceAnswer(String expression) {
+        // assign first operand, operator, and second operand to variables
         String firstFraction = parseExpressionForFirstFraction(expression);
         String operator = parseExpressionForOperator(expression);
         String secondFraction = parseExpressionForSecondFraction(expression);
@@ -27,12 +28,14 @@ public class FractionalCalculator {
     }
     
     public static String parseExpressionForFirstFraction(String expression) {
+        // method returns String containing first fraction
         int space = expression.indexOf(" ");
         String frac1 = expression.substring(0, space);
         return frac1;
     }
     
-        public static String parseExpressionForOperator(String expression) {
+    public static String parseExpressionForOperator(String expression) {
+        // method returns String containing operator
         int space = expression.indexOf(" ");
         String frac1 = expression.substring(0, space);
         String operator = expression.substring(space + 1, space + 2);
@@ -40,6 +43,7 @@ public class FractionalCalculator {
     }
     
     public static String parseExpressionForSecondFraction(String expression) {
+        // method returns String containing second fraction
         int space = expression.indexOf(" ");
         String frac1 = expression.substring(0, space);
         String operator = expression.substring(space + 1, space + 2);
@@ -52,9 +56,9 @@ public class FractionalCalculator {
         int length = fraction.length();
         int underscore = fraction.indexOf("_");
         int slash = fraction.indexOf("/");
-        String whole = "0";
-        String numerator = "0";
-        String denominator = "1";
+        String whole = "0"; // default
+        String numerator = "0"; // default
+        String denominator = "1"; // default
         if (underscore >= 0) {
             whole = fraction.substring(0, underscore);
             if (slash >= 0) { // underscore and a slash ==> mixed number
