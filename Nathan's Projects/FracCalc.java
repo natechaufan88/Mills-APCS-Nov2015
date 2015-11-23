305d6cf83d676e0ff7c62c89d8bb990d1cdc8859
natechaufan88 <natechaufan88@gmail.com>
2015-11-23 08:52:09 -0800

    FracCalc

---
+++ Nathan's Projects/FracCalc.java
@@ -0,0 +1,166 @@
+ /* Nathan Chau
+  * November 20, 2015
+  * Fractional Calculator
+  */
+
+import java.util.*; 
+ 
+public class FracCalc {
+    public static void main(String[] args) 
+    {
+        // TODO: Read the input from the user and call produceAnswer with an equation
+        Scanner userinput = new Scanner(System.in);
+        System.out.print("What is your expression? (\"quit\" to quit) "); // prompt user for input
+        String expression = userinput.nextLine(); // user types in input
+        while (!(expression.equals("quit"))) { // while user input is not "quit"
+            System.out.println(produceAnswer(expression));
+            System.out.print("What is your expression? (\"quit\" to quit) "); // prompt user for input
+            expression = userinput.nextLine(); // user types in input
+        }
+        System.out.println("You may now exit.");
+    }
+    
+    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
+    // This function takes a String 'input' and produces the result
+    //
+    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
+    //      e.g. input ==> "1/2 + 3/4"
+    //        
+    // The function should return the result of the fraction after it has been calculated
+    //      e.g. return ==> "1_1/4"
+    
+    public static String produceAnswer(String expression)
+    {
+        // TODO: Implement this function to produce the solution to the input
+        
+        // assign first operand, operator, and second operand to variables
+        String firstFraction = parseExpressionForFirstFraction(expression);
+        String operator = parseExpressionForOperator(expression);
+        String secondFraction = parseExpressionForSecondFraction(expression);
+        int firstWhole = parseFractionForWholeNumber(firstFraction);
+        int firstNumerator = parseFractionForNumerator(firstFraction);
+        int firstDenominator = parseFractionForDenominator(firstFraction);
+        int secondWhole = parseFractionForWholeNumber(secondFraction);
+        int secondNumerator = parseFractionForNumerator(secondFraction);
+        int secondDenominator = parseFractionForDenominator(secondFraction);
+        int whole = addWholes(firstWhole, secondWhole);
+        return String.valueOf(whole);
+    }
+
+    // TODO: Fill in the space below with any helper methods that you think you will need
+    public static int addWholes(int firstWhole, int secondWhole) {
+        return firstWhole + secondWhole;
+    }
+    
+    public static String parseExpressionForFirstFraction(String expression) {
+        // method returns String containing first fraction
+        int space = expression.indexOf(" ");
+        String frac1 = expression.substring(0, space);
+        return frac1;
+    }
+    
+    public static String parseExpressionForOperator(String expression) {
+        // method returns String containing operator
+        int space = expression.indexOf(" ");
+        String frac1 = expression.substring(0, space);
+        String operator = expression.substring(space + 1, space + 2);
+        return operator;
+    }
+    
+    public static String parseExpressionForSecondFraction(String expression) {
+        // method returns String containing second fraction
+        int space = expression.indexOf(" ");
+        String frac1 = expression.substring(0, space);
+        String operator = expression.substring(space + 1, space + 2);
+        int length = expression.length();
+        String frac2 = expression.substring(space + 3, length);
+        return frac2;
+    }
+    
+    public static int parseFractionForWholeNumber(String fraction) {
+        int length = fraction.length();
+        int underscore = fraction.indexOf("_");
+        int slash = fraction.indexOf("/");
+        String whole = "0"; // default
+        String numerator = "0"; // default
+        String denominator = "1"; // default
+        if (underscore >= 0) { // underscore will equal -1 if the String is not found, 
+                               // so this tests that an underscore exists in the String
+            whole = fraction.substring(0, underscore);
+            if (slash >= 0) { // underscore and a slash ==> mixed number
+                numerator = fraction.substring(underscore + 1, slash);
+                denominator = fraction.substring(slash + 1, length);
+            }
+        } else {
+            if (slash >=0) { // no underscore and a slash ==> fraction
+                whole = "0";
+                numerator = fraction.substring(0, slash);
+                denominator = fraction.substring(slash + 1, length);
+            } else { // no underscore and no slash ==> whole number
+                whole = fraction.substring(0, length);
+                numerator = "0";
+                denominator = "1";
+            }
+        }
+        int wholeNumber = Integer.parseInt(whole);
+        return wholeNumber;
+    }
+    
+    public static int parseFractionForNumerator(String fraction) {
+        int length = fraction.length();
+        int underscore = fraction.indexOf("_");
+        int slash = fraction.indexOf("/");
+        String whole = "0"; // default
+        String numerator = "0"; // default
+        String denominator = "1"; // default
+        if (underscore >= 0) { // underscore will equal -1 if the String is not found, 
+                               // so this tests that an underscore exists in the String
+            whole = fraction.substring(0, underscore);
+            if (slash >= 0) { // underscore and a slash ==> mixed number
+                numerator = fraction.substring(underscore + 1, slash);
+                denominator = fraction.substring(slash + 1, length);
+            }
+        } else {
+            if (slash >=0) { // no underscore and a slash ==> fraction
+                whole = "0";
+                numerator = fraction.substring(0, slash);
+                denominator = fraction.substring(slash + 1, length);
+            } else { // no underscore and no slash ==> whole number
+                whole = fraction.substring(0, length);
+                numerator = "0";
+                denominator = "1";
+            }
+        }
+        int numeratorNumber = Integer.parseInt(numerator);
+        return numeratorNumber;
+    }
+    
+    public static int parseFractionForDenominator(String fraction) {
+        int length = fraction.length();
+        int underscore = fraction.indexOf("_");
+        int slash = fraction.indexOf("/");
+        String whole = "0"; // default
+        String numerator = "0"; // default
+        String denominator = "1"; // default
+        if (underscore >= 0) { // underscore will equal -1 if the String is not found, 
+                               // so this tests that an underscore exists in the String
+            whole = fraction.substring(0, underscore);
+            if (slash >= 0) { // underscore and a slash ==> mixed number
+                numerator = fraction.substring(underscore + 1, slash);
+                denominator = fraction.substring(slash + 1, length);
+            }
+        } else {
+            if (slash >=0) { // no underscore and a slash ==> fraction
+                whole = "0";
+                numerator = fraction.substring(0, slash);
+                denominator = fraction.substring(slash + 1, length);
+            } else { // no underscore and no slash ==> whole number
+                whole = fraction.substring(0, length);
+                numerator = "0";
+                denominator = "1";
+            }
+        }
+        int denominatorNumber = Integer.parseInt(denominator);
+        return denominatorNumber;
+    }
+}
