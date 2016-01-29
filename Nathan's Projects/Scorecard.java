60f22e97293bd58d386aad72fd54ddf57097f2d7
natechaufan88 <natechaufan88@gmail.com>
2016-01-29 08:50:52 -0800

    Golf

--- Nathan's Projects/Scorecard.java
+++
@@ -1,8 +0,0 @@
-public class Scorecard {
-    //main
-        //scanner
-        //int parnumber = console.getnextint
-        //get next int
-        
-        
-        Scorecard player = new Scorecard();
