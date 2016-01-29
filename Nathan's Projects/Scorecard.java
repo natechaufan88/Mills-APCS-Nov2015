05044da1b59d8b7c89a9c5ec143edbee0ddd9582
natechaufan88 <natechaufan88@gmail.com>
2016-01-29 08:05:30 -0800

    sync

---
+++ Nathan's Projects/Scorecard.java
@@ -0,0 +1,8 @@
+public class Scorecard {
+    //main
+        //scanner
+        //int parnumber = console.getnextint
+        //get next int
+        
+        
+        Scorecard player = new Scorecard();
